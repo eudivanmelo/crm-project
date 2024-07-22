8d31895026b4b12915c0939d05552ead4933b997
Eudivan <eudivan44@gmail.com>
2024-07-22 20:48:09 -0300

    listview and createview to category

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views.auth import LoginView, RegisterView
+from .views.category import Category_ListView, Category_CreateView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login_page'),
     path('register/', RegisterView.as_view(), name='register_page'),
+
+    path('categories/', Category_ListView.as_view(), name='category-list'),
+    path('category/create', Category_CreateView.as_view(), name='category-create')
 ]
---
+++ main_app/views/category.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+from django.views.generic import ListView, CreateView
+from ..models import Category
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.contrib import messages
+
+class Category_ListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    model = Category
+    paginate_by = 10
+    template_name = 'category/list.html'
+    permission_required = 'main_app.view_category'
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            messages.error(self.request, 'You need to be logged in to access the category list')
+            return redirect('login_page')
+        
+        messages.error(self.request, 'You do not have permission to view the categories')
+        return redirect(self.request.META.get('HTTP_REFERER', '')) # TODO Definir página inicial
+    
+class Category_CreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    model = Category
+    fields = ['name', 'description']
+    template_name = 'category/create.html'
+    permission_required = 'main_app.add_category'
+    success_url = reverse_lazy('category-list')
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        messages.success(self.request, f"Category '{form.cleaned_data['name']}' created successfully")
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, 'There was an error creating the category. Please check the form and try again.')
+        return super().form_invalid(form)
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            messages.error(self.request, 'You need to be logged in to create a new category')
+            return redirect('login_page')
+        
+        messages.error(self.request, 'You do not have permission to add a new category')
+        return redirect(self.request.META.get('HTTP_REFERER', '')) # TODO Definir página inicial
